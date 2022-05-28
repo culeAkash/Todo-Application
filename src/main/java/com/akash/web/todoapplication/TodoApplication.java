38edf0246620f36d47ae3fde30df00a124a17d95
Akash <akashjais929@gmail.com>
2022-05-28 14:52:04 +0530

    Step 1
    Basic Spring Boot Application Setup

---
+++ src/main/java/com/akash/web/todoapplication/TodoApplication.java
@@ -0,0 +1,14 @@
+package com.akash.web.todoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SprinBootApplication initialises the Spring(Component Scan) ans Spring Boot(Auto Configuration)
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
